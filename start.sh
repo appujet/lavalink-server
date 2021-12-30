9c459e3d42141296a6174d692d70415a496f7bef
Blacky <77108939+brblacky@users.noreply.github.com>
2021-12-30 18:15:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 ## use java 13 :3
 jabba install openjdk@1.13.0-2
-npm i
-node index
+
+#This Lavalink.jar from https://ci.fredboat.com/
+curl "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev" -o Lavalink.jar
+#This Lavalink.jar by Darren Nathanael:
+#curl https://cdn.darrennathanael.com/jars/Lavalink.jar --output Lavalink.jar
+
+
+
+java -jar Lavalink.jar
