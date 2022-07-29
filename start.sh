a00d9839e85a9b9cfe18b3f7fde371257cf17099
Blacky <77108939+brblacky@users.noreply.github.com>
2022-07-29 07:43:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
+echo Downloading Java 16...
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## use java 13 :3
-jabba install openjdk@1.13.0-2
+## get java 16
+jabba install zulu@1.16.0
+echo Java is installed.
+java -version
 
 curl "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev" -o Lavalink.jar
-
+echo Lavalink.jar is downloaded!
 
 java -jar Lavalink.jar
