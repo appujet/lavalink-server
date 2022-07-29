7c9f7498146703087a24020751f401734e7b61ca
Blacky <77108939+brblacky@users.noreply.github.com>
2022-07-29 07:45:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ echo Java is installed.
 java -version
 
 curl "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev" -o Lavalink.jar
-echo Lavalink.jar is downloaded!
+echo Lavalink.jar is downloaded...
 
 java -jar Lavalink.jar
